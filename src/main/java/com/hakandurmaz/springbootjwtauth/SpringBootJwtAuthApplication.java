b4e1d9d0569dd1968cf50bf960902c76455f8493
hakandurmaz <hello@hakandurmaz.com.tr>
2020-11-02 22:40:13 +0300

    remove untracked files

--- src/main/java/com/hakandurmaz/springbootjwtauth/SpringBootJwtAuthApplication.java
+++ src/main/java/com/hakandurmaz/springbootjwtauth/SpringBootJwtAuthApplication.java
@@ -1,12 +1,7 @@
 package com.hakandurmaz.springbootjwtauth;
 
-import com.hakandurmaz.springbootjwtauth.repository.UserMongoRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.MongoDbFactory;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
